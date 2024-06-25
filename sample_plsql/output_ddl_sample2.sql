57a1567eb5c750e19086972929833d7727b1fb7d
Satoshi T <64382011+Satoshi-Tb@users.noreply.github.com>
2024-06-26 00:45:18 +0900

    親子関係順にテーブルDDL作成するように修正

--- sample_plsql/output_ddl_sample2.sql
+++ sample_plsql/output_ddl_sample2.sql
@@ -9,6 +9,67 @@ DECLARE
     -- DROP文作成
     drop_flg char(1) := '0';
 
+    CURSOR c_tables IS
+    WITH refs (child, parent, depth) AS (
+    SELECT
+        t.table_name child,
+        null parent,
+        1 AS depth
+    FROM
+        user_tables t
+    WHERE
+        NOT EXISTS (
+        SELECT *
+        FROM user_constraints fk
+        WHERE t.table_name = fk.table_name
+        AND fk.CONSTRAINT_TYPE = 'R'
+        )
+        AND t.table_name NOT LIKE 'DR$%'
+    UNION ALL
+    SELECT
+        fk.table_name,
+        r.child,
+        r.depth + 1
+    FROM
+        refs r
+    INNER JOIN user_constraints pk
+        ON pk.table_name = r.child
+    INNER JOIN user_constraints fk
+        ON fk.R_CONSTRAINT_NAME = pk.CONSTRAINT_NAME
+        AND fk.constraint_type = 'R'
+    WHERE
+        r.depth < 100
+    )
+    cycle child set isloop to 'Y' default 'N'
+    SELECT child table_name, parent, depth FROM Refs
+    WHERE isloop = 'N'
+    ORDER BY depth, parent, table_name;
+
+    -- インデックス
+    cursor c_indexes (p_table_name IN VARCHAR2)
+    IS
+    SELECT index_name
+    FROM user_indexes ix
+    WHERE ix.table_name = p_table_name
+    AND ix.index_type <> 'LOB'
+    AND NOT EXISTS (
+        SELECT *
+        FROM user_constraints cts
+        WHERE cts.constraint_name = ix.index_name
+    )
+    ORDER BY index_name;
+
+    -- コメント
+    cursor c_comment_ddl(p_table_name IN VARCHAR2)
+    IS
+    SELECT 'COMMENT ON TABLE ' || table_name || ' IS ''' || REPLACE(comments, '''', '''''') || ''';' AS comment_ddl
+    FROM user_tab_comments
+    WHERE table_name = p_table_name AND comments IS NOT NULL
+    UNION ALL
+    SELECT 'COMMENT ON COLUMN ' || table_name || '.' || column_name || ' IS ''' || REPLACE(comments, '''', '''''') || ''';'
+    FROM user_col_comments
+    WHERE table_name = 'TABLE_NAME' AND comments IS NOT NULL;
+
     -- テーブル出力順は外部制約の親から
     CURSOR c_objects IS
     WITH CHILDS (child_table, parent_table, depth) AS (
@@ -54,7 +115,7 @@ DECLARE
     WHERE
         o.OBJECT_NAME LIKE '%/_LOB/_%' ESCAPE '/'
         AND o.OBJECT_NAME NOT LIKE 'DR$%' 
-        AND o.OBJECT_TYPE IN ('TABLE', 'INDEX', 'VIEW', 'SEQUENCE', 'MATERIALIZED VIEW')
+        AND o.OBJECT_TYPE IN ('VIEW', 'SEQUENCE', 'MATERIALIZED VIEW')
         AND NOT EXISTS (
             SELECT * FROM USER_CONSTRAINTS c
             WHERE c.CONSTRAINT_NAME = o.OBJECT_NAME
@@ -74,6 +135,36 @@ BEGIN
     DBMS_METADATA.SET_TRANSFORM_PARAM(DBMS_METADATA.SESSION_TRANSFORM, 'SEGMENT_ATTRIBUTES', FALSE);
 
     DBMS_OUTPUT.PUT_LINE('SET TERMOUT ON');
+
+
+    FOR rec IN c_tables LOOP
+        object_ddl := DBMS_METADATA.GET_DDL('TABLE', rec.table_name);
+        -- 空文字を削除
+        object_ddl := TRIM(object_ddl);
+        object_ddl := REGEXP_REPLACE(object_ddl, '[[:cntrl:]]', '');
+        object_ddl := REGEXP_REPLACE(object_ddl, '"', '');
+
+        DBMS_OUTPUT.PUT_LINE(object_ddl);
+        DBMS_OUTPUT.PUT_LINE('/');
+
+        -- インデックス定義
+        FOR rec_idx IN c_indexes(rec.table_name) LOOP
+            object_ddl := DBMS_METADATA.GET_DDL('INDEX', rec_idx.index_name);
+            -- 空文字を削除
+            object_ddl := TRIM(object_ddl);
+            object_ddl := REGEXP_REPLACE(object_ddl, '[[:cntrl:]]', '');
+            object_ddl := REGEXP_REPLACE(object_ddl, '"', '');
+
+            DBMS_OUTPUT.PUT_LINE(object_ddl);
+            DBMS_OUTPUT.PUT_LINE('/');
+        END LOOP;
+
+        -- コメント定義
+        FOR rec_cmmmt IN c_comment_ddl(rec.table_name) LOOP
+            DBMS_OUTPUT.PUT_LINE(rec_cmmmt.comment_ddl);
+            DBMS_OUTPUT.PUT_LINE('/');          
+        END LOOP;
+    END LOOP;
 
     FOR rec IN c_objects LOOP
         BEGIN
