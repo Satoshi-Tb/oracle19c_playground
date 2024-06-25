23018007b0558f8f6ad09eced9617b707390ab53
Satoshi T <64382011+Satoshi-Tb@users.noreply.github.com>
2024-06-26 01:12:14 +0900

    drop文追加

--- sample_plsql/output_ddl_sample2.sql
+++ sample_plsql/output_ddl_sample2.sql
@@ -7,7 +7,7 @@ DECLARE
     object_ddl CLOB;
 
     -- DROP文作成
-    drop_flg char(1) := '0';
+    drop_flg char(1) := '1';
 
     CURSOR c_tables IS
     WITH refs (child, parent, depth) AS (
@@ -78,57 +78,6 @@ DECLARE
     WHERE sequence_name NOT LIKE 'ISEQ%'
     ORDER BY name;    
 
-    -- テーブル出力順は外部制約の親から
-    CURSOR c_objects IS
-    WITH CHILDS (child_table, parent_table, depth) AS (
-    SELECT
-        pk.table_name AS parent_table,
-        fk.table_name AS child_table,
-        1 AS depth
-    FROM
-        user_constraints fk
-        JOIN user_constraints pk
-        ON fk.r_constraint_name = pk.constraint_name
-        AND fk.constraint_type = 'R'
-    UNION ALL
-    SELECT
-        r.parent_table,
-        fk.table_name,
-        r.depth + 1
-    FROM
-        CHILDS r
-        JOIN user_constraints fk
-        ON r.child_table  = fk.table_name
-        AND fk.constraint_type = 'R'
-        JOIN user_constraints pk
-        ON fk.r_constraint_name = pk.constraint_name
-    WHERE
-        r.depth < 100
-    )
-    SELECT DISTINCT
-        CASE o.OBJECT_TYPE
-            WHEN 'TABLE' THEN 2
-            WHEN 'INDEX' THEN 3
-            WHEN 'VIEW' THEN 4
-            WHEN 'SEQUENCE' THEN 1
-            WHEN 'MATERIALIZED VIEW' THEN 5
-            ELSE 99
-        END PRI,
-        OBJECT_NAME,
-        OBJECT_TYPE,
-        NVL(DEPTH, 0) DEPTH
-    FROM USER_OBJECTS o
-    LEFT OUTER JOIN CHILDS c
-        ON o.OBJECT_NAME = c.PARENT_TABLE
-    WHERE
-        o.OBJECT_NAME LIKE '%/_LOB/_%' ESCAPE '/'
-        AND o.OBJECT_NAME NOT LIKE 'DR$%' 
-        AND o.OBJECT_TYPE IN ('VIEW', 'SEQUENCE', 'MATERIALIZED VIEW')
-        AND NOT EXISTS (
-            SELECT * FROM USER_CONSTRAINTS c
-            WHERE c.CONSTRAINT_NAME = o.OBJECT_NAME
-        )
-    ORDER BY PRI, DEPTH, OBJECT_NAME;
 BEGIN
     -- バッファサイズを無制限に設定(クライアント環境によるので注意)
     -- NULLにするとおそらく出力されなくなる
@@ -142,6 +91,20 @@ BEGIN
     DBMS_METADATA.SET_TRANSFORM_PARAM(DBMS_METADATA.SESSION_TRANSFORM, 'SEGMENT_ATTRIBUTES', FALSE);
 
     DBMS_OUTPUT.PUT_LINE('SET TERMOUT ON');
+
+    -- DROP文
+    IF drop_flg = '1' THEN
+        FOR rec IN c_seqences LOOP
+            DBMS_OUTPUT.PUT_LINE('DROP SEQUENCE ' || rec.name);
+            DBMS_OUTPUT.PUT_LINE('/');
+        END LOOP;
+
+        -- TODOテーブルは逆順で
+        FOR rec IN c_tables LOOP
+            DBMS_OUTPUT.PUT_LINE('DROP TABLE ' || rec.table_name || 'CASCADE CONSTRAINTS');
+            DBMS_OUTPUT.PUT_LINE('/');
+        END LOOP;
+    END IF;
 
     FOR rec IN c_seqences LOOP
         object_ddl := DBMS_METADATA.GET_DDL('SEQUENCE', rec.name);
@@ -183,44 +146,6 @@ BEGIN
             DBMS_OUTPUT.PUT_LINE('/');          
         END LOOP;
     END LOOP;
-
-    -- FOR rec IN c_objects LOOP
-    --     BEGIN
-    --         -- DDLを取得して出力
-    --         object_ddl := DBMS_METADATA.GET_DDL(rec.OBJECT_TYPE, rec.OBJECT_NAME);
-
-    --         -- 空文字を削除
-    --         object_ddl := TRIM(object_ddl);
-    --         object_ddl := REGEXP_REPLACE(object_ddl, '[[:cntrl:]]', '');
-    --         object_ddl := REGEXP_REPLACE(object_ddl, '"', '');
-
-    --         IF rec.OBJECT_TYPE = 'SEQUENCE' THEN
-    --             object_ddl := REGEXP_REPLACE(object_ddl, 'START WITH \d+', 'START WITH 1');
-    --         END IF;
-
-    --         DBMS_OUTPUT.PUT_LINE('PROM > ' || rec.OBJECT_NAME);  -- 出力コメント。出力サイズが大きい場合コメントアウト
-
-    --         IF drop_flg = '1' AND (
-    --             rec.OBJECT_TYPE = 'TABLE' OR
-    --             rec.OBJECT_TYPE = 'SEQUENCE' OR
-    --             rec.OBJECT_TYPE = 'VIEW' OR
-    --             rec.OBJECT_TYPE = 'MATERIALIZED VIEW'
-    --         ) THEN
-    --             DBMS_OUTPUT.PUT_LINE('DROP ' || rec.OBJECT_TYPE || ' ' || rec.OBJECT_NAME);  -- 出力コメント。出力サイズが大きい場合コメントアウト
-    --             IF rec.OBJECT_TYPE = 'TABLE' THEN
-    --                 DBMS_OUTPUT.PUT_LINE('CASCADE CONSTRAINTS');
-    --             END IF;
-    --             DBMS_OUTPUT.PUT_LINE('/');
-    --         END IF;
-
-    --         DBMS_OUTPUT.PUT_LINE(object_ddl);
-    --         DBMS_OUTPUT.PUT_LINE('/');
-    --     EXCEPTION
-    --         WHEN OTHERS THEN
-    --         DBMS_OUTPUT.PUT_LINE('Error retrieving DDL for ' || rec.object_name || ': ' || SQLERRM);
-    --     END;
-    -- END LOOP;
-
 END;
 /
 
